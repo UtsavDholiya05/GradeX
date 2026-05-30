6bf1253973c1c9654ab02a4f79431148eaf826c0
Utsav <dholiyautsav05@gmail.com>
2026-05-30 21:32:36 +0530

    update

--- Backend/app.py
+++ Backend/app.py
@@ -2,26 +2,23 @@ from fastapi import FastAPI
 from Routes.routes import router as upload_router
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-import os
 from Middlewares.auth import JWTMiddleware
 
 load_dotenv()
 
 app = FastAPI()
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "http://localhost:5174",
-        "https://*.vercel.app",
-        os.getenv("FRONTEND_URL", "http://localhost:5173")
-    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-) 
+)
 
+# JWT Middleware
 app.add_middleware(JWTMiddleware)
 
+# Routes
 app.include_router(upload_router)
